bb245ce5cfe6efea3483238f00ecfff535a65463
James Kruth <james@arsmentis.com>
2021-12-10 00:13:19 -0500

    Add excerpts

--- theme/tony-brown-news-pro-35/functions.php
+++ theme/tony-brown-news-pro-35/functions.php
@@ -7,3 +7,21 @@ function news_box_pro_child_enqueue_scripts() {
   wp_enqueue_script("captions", get_stylesheet_directory_uri()."/captions.js", array("jquery"), false, true);
 }
 add_action( 'wp_enqueue_scripts', 'news_box_pro_child_enqueue_scripts');
+
+// add excerpt filter
+
+function add_excerpt($content) {
+	if (has_excerpt()) {
+		$excerpt = get_the_excerpt();
+		$content = <<<EOT
+<div class="article-preview margin-leader margin-trailer clearfix text-center">
+	<strong>
+		$excerpt
+	</strong>
+</div>
+$content
+EOT;
+	}
+	return $content;
+}
+add_filter('the_content', 'add_excerpt');
