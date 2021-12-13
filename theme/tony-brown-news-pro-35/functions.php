35f577f52a25681e4d9cb80d97f5d575f1a6f4b9
James Kruth <james@arsmentis.com>
2021-12-13 03:10:55 -0500

    Add audio hook

--- theme/tony-brown-news-pro-35/functions.php
+++ theme/tony-brown-news-pro-35/functions.php
@@ -26,3 +26,18 @@ EOT;
 	return $content;
 }
 add_filter('the_content', 'add_authors_excerpt');
+
+function add_audio($html) {
+	$audio = get_field('featured_audio');
+	if ( $audio ) {
+		$html = <<<EOT
+<audio controls style="width: 100%">
+  	<source src="$audio" type="audio/mpeg">
+	Your browser does not support the audio element.
+</audio>
+$html
+EOT;
+	}
+	return $html;
+}
+add_filter( 'post_thumbnail_html', 'add_audio' );
