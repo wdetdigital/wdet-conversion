6618bb77722217d2dd78af90ef2cedc0441ecbf2
James Kruth <james@arsmentis.com>
2022-01-06 02:12:03 -0500

    Fix header enqueue

--- theme/tony-brown-news-pro-35/functions.php
+++ theme/tony-brown-news-pro-35/functions.php
@@ -5,7 +5,7 @@
 function news_box_pro_child_enqueue_scripts() {
   wp_enqueue_style("parent-style", get_parent_theme_file_uri("/style.css"));
   wp_enqueue_script("captions", get_stylesheet_directory_uri()."/captions.js", array("jquery"), false, true);
-  wp_enqueue_script("captions", get_stylesheet_directory_uri()."/header.js", array("jquery"), false, true);
+  wp_enqueue_script("header", get_stylesheet_directory_uri()."/header.js", array("jquery"), false, true);
 }
 add_action( 'wp_enqueue_scripts', 'news_box_pro_child_enqueue_scripts');
 
