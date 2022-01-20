2b8376a1f2895589002c2048b787e3869948e788
James Kruth <james@kruth.org>
2022-01-20 12:54:23 -0500

    Recenter header - issue #103

--- theme/tony-brown-news-pro-35/header.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(() => {
-  jQuery("#mainNav>div.container-fluid>div.row>div.col-lg-3").each((i, div) => {
-    const $div = jQuery(div);
-    $div.addClass("col-lg-1");
-    $div.removeClass("col-lg-3");
-  });
-});
